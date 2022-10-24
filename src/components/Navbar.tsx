e1db4427fef1106ae24ad5bbf17f6b4713bacb98
Mees Goudbeek <meesgoudbeek@protonmail.com>
2022-10-25 00:31:43 +0200

    small changes in styling and text

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
                 <nav className="navbar__nav">
                     <h3 className="nav__brand">
                         <Link to="/">
-                            <i className="fas fa-video"></i> TV SHOW SEARCH
+                            <i className="fas fa-video"></i> MOVIES AND SERIES
                         </Link>
                     </h3>
                     <ul className="nav__links">
